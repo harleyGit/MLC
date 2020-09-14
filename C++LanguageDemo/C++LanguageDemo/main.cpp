95716a5889fd7e3b93895498fbf1549440172bee
Harley <harley.smessage@icloud.com>
2020-09-14 21:38:40 +0800

    docs： C++ 学习

---
+++ C++LanguageDemo/C++LanguageDemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++LanguageDemo
+//
+//  Created by Harely Huang on 2020/9/14.
+//  Copyright © 2020 Harely Huang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
