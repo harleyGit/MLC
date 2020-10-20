27cce7280b98423ef7820020f6fae7e02c7eeb87
Harley <harelysoa@qq.com>
2020-10-20 10:30:38 +0800

    feat： C++  指针

--- C++LanguageDemo/C++LanguageDemo/main.cpp
+++ C++LanguageDemo/C++LanguageDemo/main.cpp
@@ -9,7 +9,51 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::cout << "<<<<<<<<<<<<<<<<<<    Start\n\n\n";
+    
+    /**/
+    int val = 5;
+    int *prt3 = (int *)0x1000;
+    prt3 = &val;
+    std::cout<<"prt3值为："<<*prt3<<"地址为："<<&prt3<<std::endl;
+    std::cout<<"val值为："<<val<<"地址为："<<&val<<std::endl;
+    
+    
+    
+    
+    
+    
+    /* 指针变量运算
+     int iVal = 10;
+     int *piVal = &iVal;
+     
+     std::cout<<"piVal指针地址原始值为："<<piVal<<std::endl;
+     piVal++;
+     std::cout<<"piVal++ 右移地址为："<<piVal<<std::endl;
+     piVal--;
+     std::cout<<"piVal-- 左移地址为："<<piVal<<std::endl;
+     piVal=piVal+3;
+     std::cout<<"piVal+3 向右移3个整数基本内存单元偏移量基本地址为："<<piVal<<std::endl;
+     */
+    
+    
+    /*指针变量及打印
+     int num1 = 10;
+     char ch1[2] = "A";
+     
+     std::cout<<"变量名称    变量值 内存地址"<<std::endl;
+     std::cout<<"-----------------------"<<std::endl;
+     
+     std::cout<<"num1"<<"\t"<<num1<<"\t"<<&num1 <<std::endl;
+     std::cout<<"ch1"<<"\t""\t"<<ch1<<"\t"<<&ch1 <<std::endl;
+     */
+    
+    
+    std::cout << "\n\n>>>>>>>>>>>>>>>>>>    End!\n";
+    system("pause");
     return 0;
 }
+
+
+
